4d0b46c9973aac597a7423599fffb8462076c545
emmanuel_chagnas <emmanuel.chagnas@viseo.com>
2020-02-20 10:49:18 +0100

    Correction build

--- lib/di/bloc_injector.dart
+++ lib/di/bloc_injector.dart
@@ -1,10 +1,12 @@
 import 'package:github_dashboard/di/bloc_module.dart';
 import 'package:inject/inject.dart';
+import 'bloc_injector.inject.dart' as g;
 
 import '../main.dart';
 
 @Injector(const [BlocModule])
 abstract class BlocInjector {
+  @provide
   MyApp get app;
 
   static final create = g.BlocInjector$Injector.create;
